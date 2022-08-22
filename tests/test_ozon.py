2261b4b7b011bb13f4533f8fd149eb889869b8c0
Валерия <101348978+valesa-st@users.noreply.github.com>
2022-08-22 22:12:20 +0300

    Create test_ozon.py

---
+++ tests/test_ozon.py
@@ -0,0 +1,16 @@
+# python -m pytest -v --driver chrome --driver-path chromedriver.exe tests/test_ozon.py
+
+
+import time
+import pytest
+import pickle
+from pages.base import WebPage
+from pages.ozon import OzonPage
+
+
+def test_scroll_down_and_contact_click(web_browser):
+    page = OzonPage(web_browser)
+    page.scroll_down()
+    page.LOCATOR_CONTACT.is_visible()
+    page.LOCATOR_CONTACT.click()
+    assert page.get_current_url() == 'https://docs.ozon.ru/common/kontakty/
